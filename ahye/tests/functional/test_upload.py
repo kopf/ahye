05407029a2ef10c83426fefaac8fc6607eab0a24
Aengus Walton <ventolin@gmail.com>
2012-04-06 17:43:29 +0200

    First commit - pylons version

---
+++ ahye/tests/functional/test_upload.py
@@ -0,0 +1,7 @@
+from ahye.tests import *
+
+class TestUploadController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='upload', action='index'))
+        # Test response...
