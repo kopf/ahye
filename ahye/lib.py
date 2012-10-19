dcf59d8c754ceab3c767445e10eb0c0152956bfb
Aengus Walton <ventolin@gmail.com>
2012-10-19 18:46:37 +0100

    get_filename -> generate_filename

--- ahye/lib.py
+++ ahye/lib.py
@@ -4,7 +4,7 @@ import os
 
 from ahye.settings import LOCAL_UPLOADS_DIR
 
-def get_filename():
+def generate_filename():
     alphanum = string.ascii_letters + string.digits
     retval = ''
     while not retval or os.path.exists(os.path.join(LOCAL_UPLOADS_DIR, retval)): 
--- ahye/views.py
+++ ahye/views.py
@@ -3,13 +3,13 @@ import os
 from flask import request, send_from_directory, url_for
 
 from ahye.settings import VDIR, LOCAL_UPLOADS_DIR
-from ahye.lib import get_filename
+from ahye.lib import generate_filename
 from ahye import app
 
 @app.route('/upload', methods=['POST'])
 def upload():
     data = request.files['imagedata']
-    filename = get_filename()
+    filename = generate_filename()
     data.save(os.path.join(LOCAL_UPLOADS_DIR, filename))
     return url_for('serve_upload', filename=filename, _external=True)
 
